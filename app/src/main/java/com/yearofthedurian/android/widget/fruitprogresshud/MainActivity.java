cc5c7b2b97d0160b9525f56d084bac1bac3fb267
Richard Koivusalo <richardkoivusalo31@gmail.com>
2020-04-08 13:02:24 +0700

    create project

---
+++ app/src/main/java/com/yearofthedurian/android/widget/fruitprogresshud/MainActivity.java
@@ -0,0 +1,14 @@
+package com.yearofthedurian.android.widget.fruitprogresshud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
