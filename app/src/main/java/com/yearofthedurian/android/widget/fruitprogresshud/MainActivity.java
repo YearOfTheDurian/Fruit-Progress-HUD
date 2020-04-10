4b8651f62dc0b7b6e28db91d7a2ee2c99ef27bee
Richard Koivusalo <richardkoivusalo31@gmail.com>
2020-04-10 22:20:57 +0700

    Untested durian progress bar indeterminate

--- app/src/main/java/com/yearofthedurian/android/widget/fruitprogresshud/MainActivity.java
+++ app/src/main/java/com/yearofthedurian/android/widget/fruitprogresshud/MainActivity.java
@@ -1,14 +1,46 @@
 package com.yearofthedurian.android.widget.fruitprogresshud;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.vectordrawable.graphics.drawable.Animatable2Compat;
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat;
 
+import com.yearofthedurian.android.widget.fruitprogresshud.FruitProgressHUD;
+import com.yearofthedurian.android.widget.fruitprogresshud.R;
+
+
+import android.content.Context;
+import android.graphics.drawable.Animatable2;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by Richard Koivusalo
+ * Copyright 2020
+ * Year of the Durian, FruitVenture LLC
+ */
 
 public class MainActivity extends AppCompatActivity {
+
+    FruitProgressHUD fruitProgressHUD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fruitProgressHUD  = (FruitProgressHUD) getSupportFragmentManager().findFragmentById(R.id.fragment_progress_hud);
+
     }
+
+
 }
---
+++ fruitprogresshud/src/main/java/com/yearofthedurian/android/widget/fruitprogresshud/FruitProgressHUD.java
@@ -0,0 +1,166 @@
+package com.yearofthedurian.android.widget.fruitprogresshud;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.vectordrawable.graphics.drawable.Animatable2Compat;
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat;
+
+/**
+ * Created by Richard Koivusalo
+ * Copyright 2020
+ * Year of the Durian, FruitVenture LLC
+ */
+
+public class FruitProgressHUD extends Fragment {
+
+    AnimatedVectorDrawableCompat animatedVectorDrawableCompat;
+    AnimatedVectorDrawableCompat animatedVectorDrawableCompatSuccess;
+    AnimatedVectorDrawableCompat animatedVectorDrawableCompatFailure;
+
+    private View mView;
+    private ImageView mImageView;
+    private TextView mTextViewComplete;
+    private TextView mTextView;
+    private boolean stopAnimation = false;
+    private int mDuration;
+    private boolean mSuccess;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_progress_hud, container, false);
+
+        mView = view.findViewById(R.id.progress_hud_background);
+        mImageView = view.findViewById(R.id.progress_hud_image_view);
+        mTextView = view.findViewById(R.id.progress_hud_text_view);
+        mTextViewComplete = view.findViewById(R.id.progress_hud_text_view_end);
+
+        mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                animate(view);
+            }
+        });
+
+        show("Loading...");
+        return view;
+    }
+
+    private void switchTextField() {
+        if (stopAnimation) {
+            mTextViewComplete.setVisibility(View.VISIBLE);
+            mTextView.setVisibility(View.INVISIBLE);
+        } else {
+            mTextViewComplete.setVisibility(View.INVISIBLE);
+            mTextView.setVisibility(View.VISIBLE);
+        }
+
+    }
+
+    private void hideHUD() {
+        mView.setVisibility(View.INVISIBLE);
+        mImageView.setVisibility(View.INVISIBLE);
+        mTextView.setVisibility(View.INVISIBLE);
+        mTextViewComplete.setVisibility(View.INVISIBLE);
+    }
+
+    private void showHUD() {
+        mView.setVisibility(View.VISIBLE);
+        mImageView.setVisibility(View.VISIBLE);
+        mTextView.setVisibility(View.VISIBLE);
+        resetHUD();
+    }
+
+    private void resetHUD() {
+        mTextView.setText("");
+        mTextViewComplete.setText("");
+    }
+
+
+    private void animate(View view) {
+        animatedVectorDrawableCompat = AnimatedVectorDrawableCompat.create(getContext(), R.drawable.avd_durian);
+        animatedVectorDrawableCompatSuccess = AnimatedVectorDrawableCompat.create(getContext(), R.drawable.avd_success_durian);
+        animatedVectorDrawableCompatFailure = AnimatedVectorDrawableCompat.create(getContext(), R.drawable.avd_failure_durian);
+
+        animatedVectorDrawableCompat.registerAnimationCallback(new Animatable2Compat.AnimationCallback() {
+            @Override
+            public void onAnimationEnd(Drawable drawable) {
+                super.onAnimationEnd(drawable);
+                if (stopAnimation) {
+                    if (mSuccess) {
+                        mImageView.setImageDrawable(animatedVectorDrawableCompatSuccess);
+                        animatedVectorDrawableCompatSuccess.start();
+                    } else {
+                        mImageView.setImageDrawable(animatedVectorDrawableCompatFailure);
+                        animatedVectorDrawableCompatFailure.start();
+                    }
+
+                } else {
+                    animatedVectorDrawableCompat.start();
+                }
+
+            }
+        });
+
+        animatedVectorDrawableCompatSuccess.registerAnimationCallback(new Animatable2Compat.AnimationCallback() {
+            @Override
+            public void onAnimationEnd(Drawable drawable) {
+                super.onAnimationEnd(drawable);
+                try {
+                    Thread.sleep(mDuration);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                hideHUD();
+                stopAnimation = false;
+            }
+        });
+
+        animatedVectorDrawableCompatFailure.registerAnimationCallback(new Animatable2Compat.AnimationCallback() {
+            @Override
+            public void onAnimationEnd(Drawable drawable) {
+                super.onAnimationEnd(drawable);
+                try {
+                    Thread.sleep(mDuration);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                hideHUD();
+                stopAnimation = false;
+            }
+        });
+
+        mImageView.setImageDrawable(animatedVectorDrawableCompat);
+        animatedVectorDrawableCompat.start();
+
+    }
+
+    public void show(String message) {
+        mTextView.setText(message);
+        animate(this.getView());
+    }
+
+    public void dismiss(String message, final int duration, boolean success) {
+        stopAnimation = true;
+        mSuccess = success;
+        switchTextField();
+        mTextViewComplete.setText(message);
+
+        mDuration = duration;
+        animatedVectorDrawableCompat.stop();
+    }
+}
